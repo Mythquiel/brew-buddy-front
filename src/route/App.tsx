ad9a95a2933c9786440cbe7d485c97cad60b8986
magdalenaswitala <ma.switala@gmail.com>
2025-10-26 22:49:38 +0100

    Fix base layout to show footer and add padding for navigation

--- src/route/App.tsx
+++ src/route/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import Home from "../pages/Home"
 import Beverages from "../pages/Beverages"
